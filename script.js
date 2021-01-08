40e6433fbd11da42fb674e1112757a806f0baf93
Thierno Souleymane Diallo <thiernosouleymane24@gmail.com>
2021-01-07 22:14:47 -0500

    create the ADD ROW

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+const addRow = () => {
+    const table = document.getElementById("table");
+    const row = document.createElement("tr");
+  
+    row.classList.add("row");
+    table.appendChild(row);
+
+    const cell = document.createElement("td");
+    cell.style.border = "2px solid black"
+    cell.style.width = "30px"
+    cell.style.padding ="10px"
+    row.appendChild(cell);
+}
