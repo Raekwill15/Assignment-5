b59c928cfc7fb1b14a26b8cc8f1a16e8ba1c53e1
capre15 <69794683+capre15@users.noreply.github.com>
2021-01-07 21:11:13 -0500

    added AddRow and AddColumn function

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+let row = 1;
+let column = 1;
+
+function addRow(){
+    let table = document.getElementById("table");
+//    table = table.getElementsByTagName("group")[0];
+    let newRow = document.createElement("tr");
+    
+    for(let i = 0; i < column; i++){
+        let cell = document.createElement("td");
+        newRow.appendChild(cell);
+    }
+    
+    table.appendChild(newRow);
+    row++;
+}
+
+function addColumn(){
+    let table = document.getElementById("table");
+//    table = table.getElementsByClassName("group")[0];
+    
+    for (let i = 0; i < row; i++) {
+        let cell = document.createElement("td");
+        table.rows[i].appendChild(cell); 
+    }
+    column++;
+}
+
 
